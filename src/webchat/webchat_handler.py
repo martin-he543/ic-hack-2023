0fac367aa59bef75692391289e8e76764507cae0
Thomas Andrews <andrewstda@outlook.com>
2023-02-04 19:02:09 +0000

    all contained in webchat

--- src/webchat/webchat_handler.py
+++ src/webchat/webchat_handler.py
@@ -1,8 +1,41 @@
 import datetime
 import json
 
-from src.utilities.helpers import TEST_WEBCHAT_DATA
 import logging
+
+
+TEST_WEBCHAT_DATA = [
+    {
+        "account_id": 1,
+        "date_time": "2020-03-20T14:28:23.382748",
+        "event_id": 1,
+        "text": "haha wow I love you uwu wow",
+    },
+    {
+        "account_id": 2,
+        "date_time": "2020-03-21T14:28:23.382748",
+        "event_id": 1,
+        "text": "awwww i totally wuv u too how wholesome",
+    },
+    {
+        "account_id": 3,
+        "date_time": "2020-03-22T14:28:23.382748",
+        "event_id": 2,
+        "text": "we must plan the destruction of the empire",
+    },
+    {
+        "account_id": 4,
+        "date_time": "2020-03-23T14:28:23.382748",
+        "event_id": 2,
+        "text": "indeed, long live the rebellion",
+    },
+    {
+        "account_id": 5,
+        "date_time": "2020-03-23T14:28:23.382748",
+        "event_id": 2,
+        "text": "oh oh, stinkyyyy",
+    },
+]
 
 
 def get_messages(event_id):
@@ -29,7 +62,7 @@ def read_handler(event, context):
 
 def send_handler(event, context):
     message = {
-        "user_id": event["account_id"],
+        "user_id": event["account_idvccf"],
         "date_time": datetime.datetime.now().isoformat(),
         "event_id": event["event_id"],
         "text": event["text"],
