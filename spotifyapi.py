763dbb9f7099f587c6bab2c2cb8674c7b26a56c0
lhollister <lhollister03@gmail.com>
2023-02-04 22:01:38 +0000

    Updated Spotify Feature - Functional.

--- spotifyapi.py
+++ spotifyapi.py
@@ -3,12 +3,10 @@ import requests
 import numpy as np
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# get token from spotify api (removed here)
-
 def get_auth_keys():
     return '', ''
 
-def initial_setup():
+def get_object():
     cid, cs = get_auth_keys()
     c_spotify=spotipy.oauth2.SpotifyClientCredentials(client_id=cid, client_secret=cs)
     get_token = c_spotify.get_access_token(check_cache=False)
@@ -16,7 +14,8 @@ def initial_setup():
     spotify = spotipy.Spotify(token)
     return spotify, token
 
-def playlist_gen(spotify, token, limit=20, market='US', seed_genres='indie', target_danceability=0.9):
+def playlist_gen(playlist_id, limit=20, market='US', seed_genres='indie', target_danceability=0.9):
+    spotify, token = get_object()
     endpoint_url = "https://api.spotify.com/v1/recommendations?"
     query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
 
@@ -25,25 +24,14 @@ def playlist_gen(spotify, token, limit=20, market='US', seed_genres='indie', tar
                             "Authorization":'Bearer '+token})
 
     json_response = response.json()
-    for i in json_response['tracks']:
-            print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+    uri_array=np.array([])
+    for info in json_response['tracks']:
+        uri_array= np.append(uri_array, info['uri'])
+    add_song(user_id, playlist_id, uri_array)
 
 
-def print_albums(spotify, uri):
-    results = spotify.artist_albums(uri, album_type='album')
-    albums = results['items']
-    while results['next']:
-        results = spotify.next(results)
-        albums.extend(results['items'])
-
-    for album in albums:
-        print(album['name'])
-
-def find_artist_uri(spotify, artist_name):
-    artist = spotify.search(q=artist_name, type="artist", limit=10)['artists']['items']
-    return artist[0]['uri']
-
-def search_func(spotify, search_string):
+def search_func(search_string):
+    spotify, token = get_object()
     song = spotify.search(q=search_string, type='track', limit=5)['tracks']['items']
     results_dict = dict()
     text_options=np.array([])
@@ -58,11 +46,33 @@ def search_func(spotify, search_string):
     results_dict['uris'] = uri_options
     return results_dict
 
-#get authentication token
-spotify, token = initial_setup()
+def search_add(results_dict, choice, playlist_id, user_id):
+    song_uri = results_dict['uris'][choice]
+    add_song(user_id, playlist_id, [song_uri])
 
-search_string='ruin amazing devil'
 
-search_results = search_func(search_string)
-print(search_results)
+def add_song(user_id, playlist_id, uri_array):
+    token,spotify = user_authentication(user_id)
+    spotify.playlist_add_items(playlist_id, uri_array)
+    
 
+def user_authentication(user_id):
+    scope_set = 'playlist-modify-private'
+    cid, cs = get_auth_keys()
+    user_auth = spotipy.oauth2.SpotifyOAuth(client_id=cid, client_secret=cs, redirect_uri='http://localhost:666', scope=scope_set, username=user_id)
+    token = user_auth.get_access_token()['access_token']
+    spotify = spotipy.Spotify(token)
+    return token, spotify
+
+
+def create_playlist(playlist_name, user_id):
+    token, spotify = user_authentication(user_id)
+    event_playlist = spotify.user_playlist_create(user_id, playlist_name, public=False, collaborative=False, description='testingabc')
+    playlist_id = event_playlist['uri']
+    return playlist_id
+
+
+def find_playlists(user_id):
+    token, spotify = user_authentication(user_id)
+    playlists = spotify.current_user_playlists()
+    print(playlists['items'][0]['name'])
